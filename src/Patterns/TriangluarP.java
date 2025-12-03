3da49a31c7e89a5d17c1524a718e94d6fffe425f
[200~wasif-codi~ <mwasif154@gmail.com>
2025-12-03 14:27:24 +0530

    patterns programs

---
+++ src/Patterns/TriangluarP.java
@@ -0,0 +1,26 @@
+package patterns;
+
+public class TriangluarP {
+    public static void main(String[] args){
+
+        // print Triangular pattern
+        // no of row is equal to no of column
+
+        for(int i = 1 ; i<= 5 ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+        //Reverse Triangular pattern
+
+//        for (int i = 5 ; i>=1 ; i--){
+//            for(int j =1 ; j<= i; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+    }
+}
