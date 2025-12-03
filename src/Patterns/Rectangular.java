4901584bd51c4e8dc5b13a43237014dd5d1ced96
[200~wasif-codi~ <mwasif154@gmail.com>
2025-12-03 14:22:46 +0530

    patterns programs

---
+++ src/Patterns/Rectangular.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class Rectangular {
+    public static void main(String[] args) {
+        // print a rectangular star pattern
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 6; j++) {
+                    System.out.print("*");
+                }
+            System.out.println();
+            }
+
+        }
+    }
+
